6fe5fb29290290a5a386f221549fa17b1afbf23d
U-Xpeiro\Dani Peiró <Dani Peir@Xpeiro.(none)>
2014-05-02 22:17:51 +0200

    Initial Commit

---
+++ tiempodejuego/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.retardo = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Play"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'retardo
+        '
+        Me.retardo.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(197, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Retardo:   0 Segundos"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(106, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 41)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(197, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Último:      0 Segundos"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(327, 6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(84, 41)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Cerrar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(420, 53)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Tiempo de Juego"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents retardo As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ tiempodejuego/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim inter As String = ""
+    Dim player As New MediaPlayer.MediaPlayer
+    Dim n As Integer = 0
+    Dim copeurl As String = "http://c21livecdn002.cires21.com/playercope.mp3"
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Button1.Text = "Pause" Then
+            player.Pause()
+            Button1.Text = "Play"
+            retardo.Start()
+        Else
+            If Not (player.PlayState = 6 Or player.PlayState = 0) Then
+                player.Play()
+                Button1.Text = "Pause"
+                retardo.Stop()
+                inter = Label1.Text.Substring(10)
+                Label2.Text = "Último:     " & inter
+            Else
+                player.FileName = copeurl
+                player.Play()
+                Button1.Text = "Pause"
+                player.Volume = 0
+            End If
+
+        End If
+
+
+    End Sub
+
+    Private Sub retardo_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles retardo.Tick
+        n = n + 1
+        Label1.Text = "Retardo:  " & n & " Segundos"
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        player.Stop()
+        Button1.Text = "Play"
+        Label1.Text = "Retardo:  0 Segundos"
+        retardo.Stop()
+        n = 0
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+End Class
